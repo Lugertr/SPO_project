98546561187e896cf85f2e1d10e2fbe43a7fa5d9
Lugertr <86018433+Lugertr@users.noreply.github.com>
2021-06-23 23:24:57 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,21 @@
 import Lexer from "./Lexer";
 import Parser from "./Parser";
+import HashTable from "./HashTable";
+import {Hash} from "./Hash";
 import StackMachine from "./StackMachine";
 
-const code = './src/TestWhile.txt';
+const code = './src/HashTest/Add_in_Hash.txt';
+//const code = './src/HashTest/ClearHash.txt';
+//const code = './src/HashTest/Get_from_Hash.txt';
+//const code = './src/HashTest/Remove.txt';
+//const code = './src/HashTest/Size.txt';
+
+//const code = './src/TestEndless.txt';
+//const code = './src/TestIf.txt';
+//const code = './src/TestIf_ELSE.txt';
+//const code = './src/TestOPeration.txt';
+//const code = './src/TestWhile.txt';
+
 
 const lexer = new Lexer(code); //создание лексера
 lexer.lexAnalysis()            //разборка кода на токены
@@ -12,6 +25,5 @@ lexer.lexAnalysis()            //разборка кода на токены
 const parser = new Parser(lexer.tokenList);     //Создание парсера
 const rootNode = parser.parseCode();            //Парсинг кода, распределение токенов по дереву,
                                                 //Их запись в стек и обработка
-
 const runCode = new StackMachine();
 runCode.run(rootNode);                           //Выполнение кода
